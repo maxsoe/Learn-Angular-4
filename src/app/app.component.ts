4bc5da4b175c59b1e963d474ba4044d4692e21b1
Max Soe <maxsoe@gmail.com>
2017-10-20 23:24:34 +0100

    Lesson 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>Hey guys!</h1>
+  <p>{{myObject.gender}}</p>
+  <ul>
+    <li *ngFor="let arr of myArr">{{arr}}</li>
+  </ul>
+
+  <div *ngIf="myArr; then tmpl1 else tmpl2">Yeah, I exist.</div>
+  <ng-template #tmpl1>I'm here</ng-template>
+  <ng-template #tmpl2>I'm note here</ng-template>
+
+  `
 })
 export class AppComponent {
-  title = 'app';
+
+  myObject = {
+    gender: 'male',
+    age: 33,
+    location: 'USA'
+  };
+
+  myArr = ['him','hers','yours','theirs'];
 }
